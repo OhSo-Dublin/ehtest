3f663ab7f0fa1c6803b1aa1f644c2cad4e485a3e
OhSo-Dublin <94744132+OhSo-Dublin@users.noreply.github.com>
2024-11-25 10:01:15 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@
  */
 const nextConfig = {
     env: {
-        stackbitPreview: process.env.STACKBIT_PREVIEW
+        stackbitPreview: process.env.STACKBIT_PREVIEW,
     },
     trailingSlash: true,
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    basePath: '/marketing', // Add this line to serve the app under /marketing
 };
 
 module.exports = nextConfig;
